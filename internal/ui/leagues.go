67547a889a2407d954d35e515108eef236bbd4d3
Matthieu Gusmini <simfy2602@outlook.fr>
2025-04-12 21:05:34 +0200

    refactor(league-selection): implement custom item delegate

--- internal/ui/leagues.go
+++ internal/ui/leagues.go
@@ -1,9 +1,12 @@
 package ui
 
 import (
+	"fmt"
+	"io"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/matthieugusmini/go-lolesports"
 )
@@ -14,34 +17,85 @@ type leagueItem struct {
 }
 
 func (i leagueItem) Title() string {
-	titleStyle := lipgloss.NewStyle().
-		Foreground(white).
-		Bold(true)
-
 	flags := strings.Join(flagsByLeagueName[i.leagueName], separatorBullet)
 	title := flags + " " + i.leagueName
-	return titleStyle.Render(title)
+	return title
 }
 
 func (i leagueItem) Description() string { return "" }
 
 func (i leagueItem) FilterValue() string { return i.leagueName }
 
+type leagueItemStyles struct {
+	normalTitle   lipgloss.Style
+	selectedTitle lipgloss.Style
+}
+
+func newDefaultLeageItemStyles() (s leagueItemStyles) {
+	s.normalTitle = lipgloss.NewStyle().
+		Padding(0, 0, 0, 2).
+		Foreground(white).
+		Bold(true)
+
+	s.selectedTitle = s.normalTitle.
+		Padding(0, 0, 0, 1).
+		Border(lipgloss.ThickBorder(), false, false, false, true).
+		BorderForeground(white)
+
+	return s
+}
+
+type leagueItemDelegate struct {
+	styles leagueItemStyles
+}
+
+func newLeagueItemDelegate() leagueItemDelegate {
+	return leagueItemDelegate{
+		styles: newDefaultLeageItemStyles(),
+	}
+}
+
+func (d leagueItemDelegate) Height() int { return 1 }
+
+func (d leagueItemDelegate) Spacing() int { return 1 }
+
+func (d leagueItemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
+	return nil
+}
+
+func (d leagueItemDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	var title string
+	if leagueItem, ok := item.(leagueItem); ok {
+		title = leagueItem.Title()
+	} else {
+		return
+	}
+
+	if m.Width() <= 0 {
+		return
+	}
+
+	isSelected := index == m.Index()
+	if isSelected {
+		title = d.styles.selectedTitle.Render(title)
+	} else {
+		title = d.styles.normalTitle.Render(title)
+	}
+	fmt.Fprint(w, title)
+}
+
 func newLeagueChoicesList(leagues []*lolesports.League, width, height int) list.Model {
-	var leagueItems []list.Item
-	for _, l := range leagues {
-		item := leagueItem{
+	leagueItems := make([]list.Item, len(leagues))
+	for i, l := range leagues {
+		leagueItems[i] = leagueItem{
 			id:         l.ID,
 			leagueName: l.Name,
 		}
-		leagueItems = append(leagueItems, item)
 	}
 
-	delegate := list.NewDefaultDelegate()
-	delegate.ShowDescription = false
-	delegate.Styles.SelectedTitle = delegate.Styles.SelectedTitle.Border(lipgloss.ThickBorder(), false, false, false, true).BorderForeground(white)
-	l := list.New(leagueItems, delegate, width, height)
+	l := list.New(leagueItems, newLeagueItemDelegate(), width, height)
 	l.Title = "LEAGUES"
+	// TODO: Where should we define this style?
 	l.Styles.Title = lipgloss.NewStyle().
 		Padding(0, 1).
 		Foreground(white).
@@ -50,6 +104,7 @@ func newLeagueChoicesList(leagues []*lolesports.League, width, height int) list.
 	l.SetShowPagination(false)
 	l.SetShowStatusBar(false)
 	l.SetShowHelp(false)
+	l.SetFilteringEnabled(false)
 
 	return l
 }
