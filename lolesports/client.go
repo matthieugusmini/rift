2a164cd6f1e517ba0065a5661f2a3a4b8bdddcfc
Matthieu Gusmini <simfy2602@outlook.fr>
2025-05-05 11:36:47 +0200

    docs: add godoc comments to the lolesports client adapter

--- lolesports/client.go
+++ lolesports/client.go
@@ -8,16 +8,19 @@ import (
 	"github.com/matthieugusmini/lolesport/timeutils"
 )
 
+// Client is an adapter of the unofficial lolesports HTTP client.
 type Client struct {
 	lolesportsClient *lolesports.Client
 }
 
+// NewClient returns a newly instanciated Client using the given lolesportsClient under the hood.
 func NewClient(lolesportsClient *lolesports.Client) *Client {
 	return &Client{
 		lolesportsClient: lolesportsClient,
 	}
 }
 
+// GetSchedule returns a lolesports.Schedule fetched from the lolesports API.
 func (c *Client) GetSchedule(
 	ctx context.Context,
 	opts *lolesports.GetScheduleOptions,
@@ -25,6 +28,7 @@ func (c *Client) GetSchedule(
 	return c.lolesportsClient.GetSchedule(ctx, opts)
 }
 
+// GetCurrentSeasonSplits returns the list of all the lolesports.Split for the current season.
 func (c *Client) GetCurrentSeasonSplits(ctx context.Context) ([]lolesports.Split, error) {
 	seasons, err := c.lolesportsClient.GetSeasons(context.Background(), nil)
 	if err != nil {
@@ -41,6 +45,7 @@ func (c *Client) GetCurrentSeasonSplits(ctx context.Context) ([]lolesports.Split
 	return currentSeason.Splits, nil
 }
 
+// GetStandings returns a list of all the standings associated with the given tournament ids.
 func (c *Client) GetStandings(
 	ctx context.Context,
 	tournamentIDs []string,
